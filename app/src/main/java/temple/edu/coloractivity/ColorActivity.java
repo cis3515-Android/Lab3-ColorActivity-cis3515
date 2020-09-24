d1edc406367098ec29a2a1e3e29b51d0787f566f
Terence21 <terencepeterson21@gmail.com>
2020-09-24 06:55:22 -0700

    created skeleton android project for color activity

---
+++ app/src/main/java/temple/edu/coloractivity/ColorActivity.java
@@ -0,0 +1,15 @@
+package temple.edu.coloractivity;
+
+import android.widget.BaseAdapter;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
