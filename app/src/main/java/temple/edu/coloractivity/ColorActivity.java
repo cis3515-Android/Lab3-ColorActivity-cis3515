fbffeb8f4804277c877896025a78bdd6b79cb819
Terence21 <terencepeterson21@gmail.com>
2020-09-24 07:55:25 -0700

    created color palatte that has 10 different colors to cycle through using ColorAdapter class

--- app/src/main/java/temple/edu/coloractivity/ColorActivity.java
+++ app/src/main/java/temple/edu/coloractivity/ColorActivity.java
@@ -1,8 +1,16 @@
 package temple.edu.coloractivity;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.ArrayList;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -10,6 +18,37 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+
+        final ArrayList<String> colors = new ArrayList<>();
+        colors.add("green");
+        colors.add("blue");
+        colors.add("red");
+        colors.add("yellow");
+        colors.add("black");
+        colors.add("cyan");
+        colors.add("darkgray");
+        colors.add("aqua");
+        colors.add("white");
+        colors.add("lime");
+
+        Spinner spinner = findViewById(R.id.spinner);
+        BaseAdapter adapter = new ColorAdapter(this, colors);
+        spinner.setAdapter(adapter);
+
+        final ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.colorSpinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                constraintLayout.setBackgroundColor(Color.parseColor(colors.get(position)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                constraintLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
+
     }
 }
--- app/src/main/java/temple/edu/coloractivity/ColorAdapter.java
+++ app/src/main/java/temple/edu/coloractivity/ColorAdapter.java
@@ -1,18 +1,32 @@
 package temple.edu.coloractivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ColorAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> colors;
+
+    public ColorAdapter(Context context, ArrayList<String> colors){
+        this.context = context;
+        this.colors = colors;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return colors.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return colors.get(position);
     }
 
     @Override
@@ -22,6 +36,19 @@ public class ColorAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        TextView textView;
+
+        if (convertView == null){
+            textView = new TextView(context);
+        } else{
+            textView = (TextView) convertView;
+        }
+
+        textView.setText(colors.get(position));
+        textView.setBackgroundColor(Color.parseColor(colors.get(position)));
+
+        return textView;
     }
+
 }
